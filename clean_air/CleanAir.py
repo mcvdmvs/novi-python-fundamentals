2f8f937ca345f247e977f03be6efc6a6fb9a920a
mcvdmvs <mick@vandermostvanspijk.nl>
2024-04-01 11:25:30 +0200

    Create CleanAir.py

---
+++ clean_air/CleanAir.py
@@ -0,0 +1,11 @@
+def CleanAir():
+  
+  def __init__(self, name):
+    self.name = name
+
+  def get_permanent_data(self, filename):
+    self.permanent_data = filename
+  
+  def get_measure_file(self, filename):
+    self.measure_file = filename
+  
