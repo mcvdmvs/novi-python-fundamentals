54c62d8dab1af09bdd683ca2df188a27b8a35262
mcvdmvs <mick@vandermostvanspijk.nl>
2024-04-01 13:58:22 +0200

    Update __main__.py

--- src/clean_air/__main__.py
+++ src/clean_air/__main__.py
@@ -1,5 +1,10 @@
+from simple_term_menu import TerminalMenu
+
 def main():
-  print('in main')
+  options=['1', '2', '3']
+  termminal_menu = TerminalMenu(options)
+  menu_entry_index= terminal_menu.show()
+  print(f"You have selected {options[menu_entry_index]}!")
 
 if __name__ == '__main__':
   main()
