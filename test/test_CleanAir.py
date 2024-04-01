a33b7fcecc4c8426a614f53e207468b01a22c45b
mcvdmvs <mick@vandermostvanspijk.nl>
2024-04-01 12:37:17 +0200

    Update test_CleanAir.py

--- test/test_CleanAir.py
+++ test/test_CleanAir.py
@@ -2,7 +2,8 @@ import pytest
 from clean_air import CleanAir
 
 class test_CleanAir():
-  self.CleanAir = CleanAir()
+  def __init(self):
+    self.CleanAir = CleanAir()
   
   def test_C1():
     assert self.CleanAir.C1 == 10
