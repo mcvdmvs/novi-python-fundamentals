a16ff08a88daae221e31fdc0abb55b71def2f6da
mcvdmvs <mick@vandermostvanspijk.nl>
2024-04-01 11:27:37 +0200

    Create Inspector.py

---
+++ clean_air/Inspector.py
@@ -0,0 +1,9 @@
+class Inspector():
+  self.code = ''
+  self.name = ''
+  self.location = ''
+
+  def __init__(self, code, name, location):
+    self.code = code
+    self.name = name
+    self.location = location
