91759e1cc1c008e1674b82c394a644fd08da2525
mcvdmvs <mick@vandermostvanspijk.nl>
2024-04-01 11:30:42 +0200

    added weight and fines as constants

--- clean_air/CleanAir.py
+++ clean_air/CleanAir.py
@@ -1,4 +1,17 @@
+import .Inspector
+
 def CleanAir():
+  # weight
+  self.C1 = 1
+  self.C2 = 2
+  self.C3 = 4
+  self.C4 = 7
+
+  # fines
+  self.F1 = 3
+  self.F2 = 10
+  self.F3 = 5
+  self.F4 = 28
   
   def __init__(self, name):
     self.name = name
