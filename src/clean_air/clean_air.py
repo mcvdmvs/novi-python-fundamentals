a6482c98a9ef8d17b5e21c36cdbe855e3524e559
mcvdmvs <mick@vandermostvanspijk.nl>
2024-04-05 15:43:14 +0200

    example calculation method

--- src/clean_air/clean_air.py
+++ src/clean_air/clean_air.py
@@ -16,6 +16,13 @@ class CleanAir:
     F3 = 5
     F4 = 28
 
+    # files
+    measure_file = None
+    permanent_data = []
+    
+    # measurements
+    measurements = {}
+    
     def __init__(self):
         pass
 
@@ -24,3 +31,13 @@ class CleanAir:
 
     def get_measure_file(self, filename):
         self.measure_file = filename
+        
+    def readMeasureData(self):
+        self.measurements = {}
+    
+    def calcEmissions(self):
+        total = 0
+        for row in self.measurements:
+            total += self.CO2 * row[CO2] + self.CH4 * row[CH4] + self.NO2 * row[NO2] + self.NH3 * row[NH3]
+        
+        return total
--- tests/test_CleanAir.py
+++ tests/test_CleanAir.py
@@ -10,3 +10,8 @@ def my_clean_air():
 
 def test_CO2_weight(my_clean_air):
     assert my_clean_air.CO2 == 1
+
+def test_calcEmissions(my_clean_air):
+    my_clean_air.measurements = [{"CO2": 2, "CH4": 4, "NO2": 54, "NH3": 7}]
+    total = my_clean_air.calcEmissions()
+    assert total == 3
