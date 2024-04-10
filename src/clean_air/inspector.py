ba16fc364c5d6e3f554d93aad6a652e9da874656
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2024-04-10 20:08:04 +0200

    String

--- src/clean_air/inspector.py
+++ src/clean_air/inspector.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass
 
+"""
+Use the dataclass decorator to create a class with a namedtuple-like interface.
+https://docs.python.org/3/library/dataclasses.html
+"""
+
 
 @dataclass
 class Inspector:
